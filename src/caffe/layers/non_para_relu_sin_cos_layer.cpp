65f6e425703e8430c8909028db5e9634bee60fac
Eddie Zhou <eddiedzhou@gmail.com>
2016-04-03 14:56:19 -0400

    post-conclusion commit

--- src/caffe/layers/non_para_relu_sin_cos_layer.cpp
+++ src/caffe/layers/non_para_relu_sin_cos_layer.cpp
@@ -54,8 +54,8 @@ void NonParaReLUSinCosLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& botto
 
   // Propagate gradients to the parameters (as directed by backward pass).
   this->param_propagate_down_.resize(this->blobs_.size(), true);
-  // multiplier_sin_.Reshape(vector<int>(1, bottom[0]->count() / bottom[0]->num()));
-  // caffe_set(multiplier_sin_.count(), Dtype(1), multiplier_sin_.mutable_cpu_data());
+  multiplier_sin_.Reshape(vector<int>(1, bottom[0]->count() / bottom[0]->num()));
+  caffe_set(multiplier_sin_.count(), Dtype(1), multiplier_sin_.mutable_cpu_data());
   multiplier_cos_.Reshape(vector<int>(1, bottom[0]->count() / bottom[0]->num()));
   caffe_set(multiplier_cos_.count(), Dtype(1), multiplier_cos_.mutable_cpu_data());
 }
@@ -80,8 +80,8 @@ void NonParaReLUSinCosLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bott
   const int count = bottom[0]->count();
   const int dim = bottom[0]->count(2);
   const int channels = bottom[0]->channels();
-  // const Dtype* beta_sin_data = this->blobs_[0]->cpu_data();
-  const Dtype* beta_cos_data = this->blobs_[0]->cpu_data();
+  const Dtype* beta_sin_data = this->blobs_[0]->cpu_data();
+  const Dtype* beta_cos_data = this->blobs_[1]->cpu_data();
 
   // For in-place computation
   if (bottom[0] == top[0]) {
@@ -94,7 +94,7 @@ void NonParaReLUSinCosLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bott
   for (int i = 0; i < count; ++i) {
     int c = (i / dim) % channels / div_factor;
     top_data[i] = std::max(bottom_data[i], Dtype(0))
-        // + beta_sin_data[c] * sin(bottom_data[i])
+        + beta_sin_data[c] * sin(bottom_data[i])
         + beta_cos_data[c] * cos(bottom_data[i]);
   }
 }
@@ -104,8 +104,8 @@ void NonParaReLUSinCosLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top
     const vector<bool>& propagate_down,
     const vector<Blob<Dtype>*>& bottom) {
   const Dtype* bottom_data = bottom[0]->cpu_data();
-  // const Dtype* beta_sin_data = this->blobs_[0]->cpu_data();
-  const Dtype* beta_cos_data = this->blobs_[0]->cpu_data();
+  const Dtype* beta_sin_data = this->blobs_[0]->cpu_data();
+  const Dtype* beta_cos_data = this->blobs_[1]->cpu_data();
   const Dtype* top_diff = top[0]->cpu_diff();
   const int count = bottom[0]->count();
   const int dim = bottom[0]->count(2);
@@ -124,28 +124,26 @@ void NonParaReLUSinCosLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top
   // Since to write bottom diff will affect top diff if top and bottom blobs
   // are identical (in-place computaion), we first compute param backward to
   // keep top_diff unchanged.
-  // if (this->param_propagate_down_[0] && this->param_propagate_down_[1]) {
-  if (this->param_propagate_down_[0]) {
-    // Dtype* beta_sin_diff = this->blobs_[0]->mutable_cpu_diff();
-    Dtype* beta_cos_diff = this->blobs_[0]->mutable_cpu_diff();
-    // caffe_set(this->blobs_[0]->count(), Dtype(0), beta_sin_diff);
-    caffe_set(this->blobs_[0]->count(), Dtype(0), beta_cos_diff);
+  if (this->param_propagate_down_[0] && this->param_propagate_down_[1]) {
+    Dtype* beta_sin_diff = this->blobs_[0]->mutable_cpu_diff();
+    Dtype* beta_cos_diff = this->blobs_[1]->mutable_cpu_diff();
+    caffe_set(this->blobs_[0]->count(), Dtype(0), beta_sin_diff);
+    caffe_set(this->blobs_[1]->count(), Dtype(0), beta_cos_diff);
     for (int i = 0; i < count; ++i) {
       int c = (i / dim) % channels / div_factor;
-      // beta_sin_diff[c] += top_diff[i] * sin(bottom_data[i]);
+      beta_sin_diff[c] += top_diff[i] * sin(bottom_data[i]);
       beta_cos_diff[c] += top_diff[i] * cos(bottom_data[i]);
     }
   }
 
   // Propagate to bottom
-  // if (propagate_down[0] && propagate_down[1]) {
   if (propagate_down[0]) {
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
     for (int i = 0; i < count; ++i) {
       int c = (i / dim) % channels / div_factor;
       bottom_diff[i] = top_diff[i] * (
           ( bottom_data[i] > 0)
-          // + beta_sin_data[c] * cos(bottom_data[i])
+          + beta_sin_data[c] * cos(bottom_data[i])
           - beta_cos_data[c] * sin(bottom_data[i]));
     }
   }
--- src/caffe/layers/non_para_relu_sin_cos_layer.cu
+++ src/caffe/layers/non_para_relu_sin_cos_layer.cu
@@ -11,13 +11,13 @@ namespace caffe {
 template <typename Dtype>
 __global__ void NonParaReLUSinCosForward(const int n, const int channels, const int dim,
     const Dtype* in, Dtype* out, 
-    // const Dtype* beta_sin_data,
+    const Dtype* beta_sin_data,
     const Dtype* beta_cos_data,
     const int div_factor) {
   CUDA_KERNEL_LOOP(index, n) {
     int c = (index / dim) % channels / div_factor;
     out[index] = (in[index] > 0 ? in[index] : 0)
-                 // + beta_sin_data[c] * sin(in[index])
+                 + beta_sin_data[c] * sin(in[index])
                  + beta_cos_data[c] * cos(in[index]);
   }
 }
@@ -26,14 +26,14 @@ __global__ void NonParaReLUSinCosForward(const int n, const int channels, const 
 template <typename Dtype>
 __global__ void NonParaReLUSinCosBackward(const int n, const int channels, const int dim,
     const Dtype* in_diff, const Dtype* in_data, Dtype* out_diff,
-    // const Dtype* beta_sin_data,
+    const Dtype* beta_sin_data,
     const Dtype* beta_cos_data,
     const int div_factor) {
   CUDA_KERNEL_LOOP(index, n) {
     int c = (index / dim) % channels / div_factor;
     out_diff[index] = in_diff[index] * (
         ( in_data[index] > 0)
-        // + beta_sin_data[c] * cos(in_data[index])
+        + beta_sin_data[c] * cos(in_data[index])
         - beta_cos_data[c] * sin(in_data[index]));
   }
 }
@@ -42,10 +42,10 @@ __global__ void NonParaReLUSinCosBackward(const int n, const int channels, const
 template <typename Dtype>
 __global__ void NonParaReLUSinCosParamBackward(const int n, const Dtype* in_diff,
     const Dtype* in_data,
-    // Dtype* out_diff_sin,
+    Dtype* out_diff_sin,
     Dtype* out_diff_cos) {
   CUDA_KERNEL_LOOP(index, n) {
-    // out_diff_sin[index] = in_diff[index] * sin(in_data[index]);
+    out_diff_sin[index] = in_diff[index] * sin(in_data[index]);
     out_diff_cos[index] = in_diff[index] * cos(in_data[index]);
   }
 }
@@ -58,8 +58,8 @@ void NonParaReLUSinCosLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bott
   const int count = bottom[0]->count();
   const int dim = bottom[0]->count(2);
   const int channels = bottom[0]->channels();
-  // const Dtype* beta_sin_data = this->blobs_[0]->gpu_data();
-  const Dtype* beta_cos_data = this->blobs_[0]->gpu_data();
+  const Dtype* beta_sin_data = this->blobs_[0]->gpu_data();
+  const Dtype* beta_cos_data = this->blobs_[1]->gpu_data();
   const int div_factor = channel_shared_ ? channels : 1;
 
   // For in-place computation
@@ -70,7 +70,7 @@ void NonParaReLUSinCosLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bott
   // NOLINT_NEXT_LINE(whitespace/operators)
   NonParaReLUSinCosForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
       count, channels, dim, bottom_data, top_data,
-      // beta_sin_data,
+      beta_sin_data,
       beta_cos_data,
       div_factor);
   CUDA_POST_KERNEL_CHECK;
@@ -95,19 +95,18 @@ void NonParaReLUSinCosLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top
   // Since to write bottom diff will affect top diff if top and bottom blobs
   // are identical (in-place computaion), we first compute param backward to
   // keep top_diff unchanged.
-  // if (this->param_propagate_down_[0] && this->param_propagate_down_[1]) {
-  if (this->param_propagate_down_[0]) {
-    // Dtype* beta_sin_diff = this->blobs_[0]->mutable_gpu_diff();
-    Dtype* beta_cos_diff = this->blobs_[0]->mutable_gpu_diff();
+  if (this->param_propagate_down_[0] && this->param_propagate_down_[1]) {
+    Dtype* beta_sin_diff = this->blobs_[0]->mutable_gpu_diff();
+    Dtype* beta_cos_diff = this->blobs_[1]->mutable_gpu_diff();
 
     // beta_sin_diff, beta_cos_diff are set as 0, then accumulated over batches
-    // caffe_gpu_set<Dtype>(this->blobs_[0]->count(), Dtype(0), beta_sin_diff);
-    caffe_gpu_set<Dtype>(this->blobs_[0]->count(), Dtype(0), beta_cos_diff);
+    caffe_gpu_set<Dtype>(this->blobs_[0]->count(), Dtype(0), beta_sin_diff);
+    caffe_gpu_set<Dtype>(this->blobs_[1]->count(), Dtype(0), beta_cos_diff);
     int cdim = channels * dim;
     Dtype dsum_sin = 0.;
     Dtype dsum_cos = 0.;
     for (int n = 0; n < bottom[0]->num(); ++n) {
-      // Dtype* temp_buff_sin = multiplier_sin_.mutable_gpu_diff();
+      Dtype* temp_buff_sin = multiplier_sin_.mutable_gpu_diff();
       Dtype* temp_buff_cos = multiplier_cos_.mutable_gpu_diff();
       // compute element-wise diff
       // NOLINT_NEXT_LINE(whitespace/operators)
@@ -115,22 +114,22 @@ void NonParaReLUSinCosLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top
           CAFFE_CUDA_NUM_THREADS>>>(
           cdim, top_diff + top[0]->offset(n),
           bottom_data + bottom[0]->offset(n),
-          // multiplier_sin_.mutable_gpu_diff(),
+          multiplier_sin_.mutable_gpu_diff(),
           multiplier_cos_.mutable_gpu_diff());
       CUDA_POST_KERNEL_CHECK;
       if (channel_shared_) {
-        // Dtype d_sin;
-        // caffe_gpu_dot<Dtype>(channels * dim, multiplier_sin_.gpu_diff(),
-        //     multiplier_sin_.gpu_data(), &d_sin);
-        // dsum_sin += d_sin;
+        Dtype d_sin;
+        caffe_gpu_dot<Dtype>(channels * dim, multiplier_sin_.gpu_diff(),
+            multiplier_sin_.gpu_data(), &d_sin);
+        dsum_sin += d_sin;
         Dtype d_cos;
         caffe_gpu_dot<Dtype>(channels * dim, multiplier_cos_.gpu_diff(),
             multiplier_cos_.gpu_data(), &d_cos);
         dsum_cos += d_cos;
       } else {
-        // caffe_gpu_gemv<Dtype>(CblasNoTrans, channels, dim, 1.,
-        //     multiplier_sin_.gpu_diff(), multiplier_sin_.gpu_data(), 1.,
-        //     beta_sin_diff);
+        caffe_gpu_gemv<Dtype>(CblasNoTrans, channels, dim, 1.,
+            multiplier_sin_.gpu_diff(), multiplier_sin_.gpu_data(), 1.,
+            beta_sin_diff);
         caffe_gpu_gemv<Dtype>(CblasNoTrans, channels, dim, 1.,
             multiplier_cos_.gpu_diff(), multiplier_cos_.gpu_data(), 1.,
             beta_cos_diff);
@@ -138,23 +137,22 @@ void NonParaReLUSinCosLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top
     }
 
     if (channel_shared_) {
-      // caffe_gpu_set(this->blobs_[0]->count(), Dtype(dsum_sin), beta_sin_diff);
-      caffe_gpu_set(this->blobs_[0]->count(), Dtype(dsum_cos), beta_cos_diff);
+      caffe_gpu_set(this->blobs_[0]->count(), Dtype(dsum_sin), beta_sin_diff);
+      caffe_gpu_set(this->blobs_[1]->count(), Dtype(dsum_cos), beta_cos_diff);
     }
   }
 
   // Propagate to bottom
-  // if (propagate_down[0] && propagate_down[1]) {
   if (propagate_down[0]) {
     Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
-    // const Dtype* beta_sin_data = this->blobs_[0]->gpu_data();
-    const Dtype* beta_cos_data = this->blobs_[0]->gpu_data();
+    const Dtype* beta_sin_data = this->blobs_[0]->gpu_data();
+    const Dtype* beta_cos_data = this->blobs_[1]->gpu_data();
     int div_factor = channel_shared_ ? channels : 1;
     // NOLINT_NEXT_LINE(whitespace/operators)
     NonParaReLUSinCosBackward<Dtype><<<CAFFE_GET_BLOCKS(count),
         CAFFE_CUDA_NUM_THREADS>>>(
         count, channels, dim, top_diff, bottom_data, bottom_diff,
-        // beta_sin_data,
+        beta_sin_data,
         beta_cos_data,
         div_factor);
     CUDA_POST_KERNEL_CHECK;
